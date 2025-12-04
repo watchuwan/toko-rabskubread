617ab66d5460b0058d0d68e147a1b2de60128dff
damian <rezalkaryanto@gmail.com>
2025-12-04 12:22:08 +0900

    update: navigation menu group

--- app/Filament/Resources/Pelanggan/PelangganResource.php
+++ app/Filament/Resources/Pelanggan/PelangganResource.php
@@ -15,11 +15,13 @@ use Filament\Resources\Resource;
 use Filament\Schemas\Schema;
 use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Table;
+use UnitEnum;
 
 class PelangganResource extends Resource
 {
     protected static ?string $model = Pelanggan::class;
 
+        protected static string | UnitEnum | null $navigationGroup = 'Manajemen Toko';
     protected static ?string $modelLabel = 'Pelanggan';
     protected static ?string $pluralModelLabel = 'Pelanggan';
     protected static ?string $navigationLabel = 'Pelanggan';
--- app/Filament/Resources/Produk/ProdukResource.php
+++ app/Filament/Resources/Produk/ProdukResource.php
@@ -17,10 +17,13 @@ use Filament\Resources\Resource;
 use Filament\Schemas\Schema;
 use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Table;
+use UnitEnum;
 
 class ProdukResource extends Resource
 {
     protected static ?string $model = Produk::class;
+
+    protected static string | UnitEnum | null $navigationGroup = 'Manajemen Toko';
 
     protected static ?string $modelLabel = 'Produk';
     protected static ?string $pluralModelLabel = 'Produk';
